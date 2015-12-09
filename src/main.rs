360d76d458a9a92b250343189c02beccd0f45524
John Barker <jebarker@gmail.com>
2015-12-09 17:36:31 -0500

    Add example websocket server using ws-rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+extern crate ws;
 #[macro_use] extern crate conv;
 #[macro_use] extern crate custom_derive;
+
 mod board;
 
+use ws::listen;
+
 fn main() {
+    if let Err(error) = listen("127.0.0.1:3012", |out| {
+        move |msg| {
+            println!("Server got message '{}'. ", msg);
+            out.send(msg)
+        }
+    }) {
+        println!("Failed to create WebSocket due to {:?}", error);
+    }
 }
